4c699d3cd46790ee1db05d01f3902858146c69b2
andherpaulo <andher.dev@gmail.com>
2022-10-05 16:42:10 -0300

    Criação dos arquivos iniciais

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import GlobalStyle from "./styles/GlobalStyle";
+
+function App() {
+    return (
+        <div>
+            <GlobalStyle />
+            App
+        </div>
+    );
+}
+
+export default App;
